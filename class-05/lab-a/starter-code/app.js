f9e8f78d941844667842c4dadf408387ee4d2753
mohammad samara <mohammad.ab.samara@gmail.com>
2020-06-11 13:48:35 +0300

    p1 solved

--- class-05/lab-a/starter-code/app.js
+++ class-05/lab-a/starter-code/app.js
@@ -13,7 +13,7 @@ return [a+b , "The sum of 4 and 7 is 11."]
 }
 
 // Here is the test for sum(); uncomment it to run it
-// testSum(4, 7);
+ testSum(4, 7);
 
 // Once you get the test passing, do an a-c-p cycle and synchronize the code between GitHub and your laptop. Don't forget to create a new branch for your work on the next question!
 
